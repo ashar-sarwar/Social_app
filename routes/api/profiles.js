b4dcaf385964b3a5970a59a86af0a3d5eedae4eb
Ashar Sarwar <asharsarwar186@gmail.com>
2019-12-14 01:08:24 +0500

    create N update profile route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,23 +1,71 @@
-const express = require('express');
-const router = express.Router()
-const mongoose = require('mongoose');
-const passport = require('passport');
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-router.get("/test",()=>console.log("profiles here"))
+router.get("/test", () => console.log("profiles here"));
 
-router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
-const errors = {}
-    Profile.findOne({user:req.user.id})
-.then(profile=>{
-    if(!profile){
-        errors.noProfile='No profile of this user'
-        return res.status(400).json(errors)
+//current user profile  --> GET Req
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = "No profile of this user";
+          return res.status(400).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(error => res.status(404).json(error));
+  }
+);
+
+//create user profile  -->POST Req
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    const profileFields = {};
+    profileFields.user = req.user.id; //the logged_in user
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.githubProfile)
+      profileFields.githubProfile = req.body.githubProfile;
+
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
     }
-    res.json(profile)
-})
-.catch(error=>res.status(404).json(error))
-})
 
-module.exports = router
+    profileFields.social = {};
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "Handle exists";
+            res.status(400).json(errors);
+          }
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+  }
+);
+
+module.exports = router;
